6fc44910a9bb19c25d9952ffad0f2961c6db7d28
piialairi <piia.lairi@myy.haaga-helia.fi>
2024-02-22 15:42:23 +0200

    Changed the password in demo data for Transport 1 and put it on the frontpage

--- src/main/webapp/app/modules/account/register/register.tsx
+++ src/main/webapp/app/modules/account/register/register.tsx
@@ -118,7 +118,8 @@ export const RegisterPage = () => {
               <Translate contentKey="global.messages.info.authenticated.suffix">
                 , you can try the default accounts:
                 <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;)
-                <br />- User (login=&quot;user&quot; and password=&quot;user&quot;).
+                <br />- User (login=&quot;user&quot; and password=&quot;user&quot;)
+                <br />- Transport 1 (login=&quot;transport1&quot; and password=&quot;user&quot;).
               </Translate>
             </span>
           </Alert>
