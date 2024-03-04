3d137e0f8ca53795db8b0fe998d2b5aa6d2b4972
zokaas <bozic.zorana@gmail.com>
2024-03-04 10:09:32 +0200

    show company name in invoice

--- src/main/webapp/app/entities/invoice/invoice.tsx
+++ src/main/webapp/app/entities/invoice/invoice.tsx
@@ -140,7 +140,7 @@ export const Invoice = () => {
                   <td>
                     {invoice.invoiceDate ? <TextFormat type="date" value={invoice.invoiceDate} format={APP_LOCAL_DATE_FORMAT} /> : null}
                   </td>
-                  <td>{invoice.company ? <Link to={`/company/${invoice.company.id}`}>{invoice.company.id}</Link> : ''}</td>
+                  <td>{invoice.company ? <Link to={`/company/${invoice.company.id}`}>{invoice.company.companyName}</Link> : ''}</td>
                   <td className="text-end">
                     <div className="btn-group flex-btn-group-container">
                       <Button tag={Link} to={`/invoice/${invoice.id}`} color="info" size="sm" data-cy="entityDetailsButton">
