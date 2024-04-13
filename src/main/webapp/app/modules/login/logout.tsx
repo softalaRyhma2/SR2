7bbee37b163e82d65d9fc5f3ec2d47af2009ff36
piialairi <piia.lairi@myy.haaga-helia.fi>
2024-04-13 23:08:20 +0300

    Redirect after logout

--- src/main/webapp/app/modules/login/logout.tsx
+++ src/main/webapp/app/modules/login/logout.tsx
@@ -1,28 +1,77 @@
-import React, { useLayoutEffect } from 'react';
-
+import React, { useLayoutEffect, useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from 'app/config/store';
 import { logout } from 'app/shared/reducers/authentication';
+import { Button } from 'reactstrap';
+import { Translate, log } from 'react-jhipster';
 
 export const Logout = () => {
   const logoutUrl = useAppSelector(state => state.authentication.logoutUrl);
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const [loggedOut, setLoggedOut] = useState(false);
+  const [secondsRemaining, setSecondsRemaining] = useState(10);
+  const [loading, setLoading] = useState(true);
 
   useLayoutEffect(() => {
     dispatch(logout());
     if (logoutUrl) {
       window.location.href = logoutUrl;
+    } else {
+      const interval = setInterval(() => {
+        setSecondsRemaining(prevSeconds => prevSeconds - 1);
+      }, 1000);
+
+      setTimeout(() => {
+        setLoggedOut(true);
+        setLoading(false);
+        navigate('/');
+        clearInterval(interval);
+      }, secondsRemaining * 1000);
+
+      return () => clearInterval(interval);
     }
-  });
+  }, [dispatch, navigate, logoutUrl]);
 
   const centerStyle = {
     display: 'flex', // Enables Flexbox
     justifyContent: 'center', // Centers horizontally
     alignItems: 'center', // Centers vertically
   };
+
+  const redirectStyle = {
+    marginTop: '20px', // Top margin for redirect container
+  };
+
   return (
-    <div style={centerStyle}>
-      <h4>Logged out successfully!</h4>
-    </div>
+    <>
+      <div style={centerStyle}>
+        {!loggedOut && (
+          <div style={{ textAlign: 'center' }}>
+            <h4>
+              <Translate contentKey="authentication.logoutSuccess">Logged out successfully!</Translate>
+              <br />
+              <Translate contentKey="authentication.redirectMessage">You will be redirected to the main page in</Translate>{' '}
+              {secondsRemaining} <Translate contentKey="authentication.seconds">seconds</Translate>.
+            </h4>
+          </div>
+        )}
+      </div>
+      <div style={redirectStyle}>
+        {!loggedOut && (
+          <div style={{ textAlign: 'center' }}>
+            <h4>
+              <Translate contentKey="authentication.redirectButton">Or press the button</Translate>
+            </h4>
+            <Link to="/">
+              <Button color="primary" size="lg" className="home-button">
+                <Translate contentKey="global.menu.home">Home</Translate>
+              </Button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
