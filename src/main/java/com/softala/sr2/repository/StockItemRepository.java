19a05e69e65c1c54345b0a8c7de3cfc0f84e2d02
Joonas Montonen <joonas.montonen@outlook.com>
2024-04-24 23:17:58 +0300

    user cant create a new stock item in a stock if there already exists an item with the same type

--- src/main/java/com/softala/sr2/repository/StockItemRepository.java
+++ src/main/java/com/softala/sr2/repository/StockItemRepository.java
@@ -2,7 +2,9 @@ package com.softala.sr2.repository;
 
 import com.softala.sr2.domain.Stock;
 import com.softala.sr2.domain.StockItem;
+import com.softala.sr2.domain.StockItemType;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
@@ -14,4 +16,5 @@ import org.springframework.stereotype.Repository;
 public interface StockItemRepository extends JpaRepository<StockItem, Long> {
     List<StockItem> findByStockId(Long id);
     List<StockItem> findByStock(Stock stock);
+    Optional<StockItem> findByStockItemType(StockItemType stockItemType);
 }
